38e91ec11203ea3de22a8db987e738ad1580e6ed
Diyorbek <majidovdiyorbek741@gmail.com>
2025-12-26 14:25:24 +0500

    project setting and url fixed

--- core/urls.py
+++ core/urls.py
@@ -2,14 +2,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
-from django.views.i18n import set_language
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('', include('courses.urls')),
 ]
 
+# Media files (development)
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Custom error handlers
+handler404 = 'courses.views.handler404'
+handler500 = 'courses.views.handler500'
