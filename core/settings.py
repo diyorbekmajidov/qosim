f218a2d37498494ca1803e2111e71285ffe8fbd1
Diyorbek <majidovdiyorbek741@gmail.com>
2025-12-26 15:07:17 +0500

    database used postgresql

--- core/settings.py
+++ core/settings.py
@@ -62,10 +62,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'qosim',
+        'USER': 'qosim',
+        'PASSWORD': 'qosim',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
